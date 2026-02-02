073c62aef6ba0ceab037b22ada0c9b8e77f3c855
rahulsewapariyar <rahulsewapariyar@gmail.com>
2026-02-02 10:40:17 +0545

    feat: setup files added

---
+++ Pokherse/src/main/java/com/example/Pokherse/PokherseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pokherse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokherseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokherseApplication.class, args);
+	}
+
+}
